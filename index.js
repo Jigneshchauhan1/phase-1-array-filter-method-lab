5893daaaf4f96499589006b6f3f0434060d4ce66
Jignesh <jigneshmailbag@gmail.com>
2021-10-04 19:43:06 -0400

    commit tree b12785f8d964d1d2d51627cf0fcd247dd7d6fbde

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+const { arrayContaining } = require("expect");
+
+function findMatching(arr, query) {
+  return arr.filter(function(elements) {
+    return elements.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  })
+}
+function fuzzyMatch(arr, query){
+  return arr.filter(function(elements) {
+    return elements.toLowerCase().indexOf(query.toLowerCase()) == 0
+  })
+}
+
+function matchName (arr, query){
+  return arr.filter(function(el){
+    return el.name == query;
+  })
+}
